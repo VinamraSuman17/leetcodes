64d0cb326b9ba8f453dd48f7af46454fb4308a2e
vinamra suman <vinamrasuman33@gmail.com>
2026-05-14 16:47:46 +0530

    prefix sum

---
+++ 523. Continuous Subarray Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+
+        unordered_map<int, int> mp;
+
+        mp[0] = -1;
+
+        int prefixSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            prefixSum += nums[i];
+
+            int rem = prefixSum % k;
+
+            if (mp.find(rem) != mp.end()) {
+
+                // subarray length should be at least 2
+                if (i - mp[rem] >= 2) {
+                    return true;
+                }
+            } else {
+                // store first occurrence only
+                mp[rem] = i;
+            }
+        }
+
+        return false;
+    }
+};
