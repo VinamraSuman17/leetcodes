01626c14f4dca02dc11b10f5822f08b48113ae01
vinamra suman <vinamrasuman33@gmail.com>
2026-05-25 11:07:10 +0530

    Jump game can alos be solved with bfs

---
+++ 1871. Jump Game VII.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+
+        int n = s.size();
+
+        queue<int> q;
+
+        vector<bool> vis(n, false);
+
+        q.push(0);
+
+        vis[0] = true;
+
+        int farthest = 0;
+
+        while(!q.empty()) {
+
+            int i = q.front();
+            q.pop();
+
+            if(i == n - 1)
+                return true;
+
+            int start = max(i + minJump, farthest + 1);
+
+            int end = min(i + maxJump, n - 1);
+
+            for(int j = start; j <= end; j++) {
+
+                if(s[j] == '0' && !vis[j]) {
+
+                    vis[j] = true;
+
+                    q.push(j);
+                }
+            }
+
+            farthest = end;
+        }
+
+        return false;
+    }
+};
